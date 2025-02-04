3ad3784952694db78cae2509f033787e0930ca07
Nuthan Kishore <nuthankishore@hotmail.com>
2025-02-04 15:03:01 +0530

    updated JSX practice changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Counter from './Counter';  
 
 function App() {
   return (
-    <main>
-      <>
-      <div style={{border: '1px solid white',margin: 'auto',marginTop: '50px'}}>
-      <div style={{background: 'black',height: '70px',display: 'flex',justifyContent: 'left',alignItems: 'center',paddingLeft: '10px'}}>
-      <a href='#' ><img src={logo} width="80px" alt="React logo"/></a>
-      <h1 style={{color:'white',fontFamily:'Verdana'}}>Welcome</h1>
+    <div className="App" id='MainDiv'>
+      <header style={{display:'flex',paddingTop:'20px'}}>
+        <img src={logo} id="logoId" alt="React Logo"/>
+        <span>ReactFacts</span>
+      </header>
+      <h2 className="text-2xl font-bold mb-4">Fun facts about React</h2>
+      <p className="text-gray-300 mb-4">
+        React is a popular JavaScript library for building user interfaces. Here are some fun facts about React:</p>
+        {/* <div className="bg-black absolute inset-0 bg-center bg-cover animate-spin-slow" background-image="url({logo})">
+        <img src={logo} id="logobg" alt="React Logo" sizes='60px'/>
+        </div> */}
+      <ul className="list-disc list-inside space-y-2 text-gray-300 mb-4">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 200K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+      <div>
+        <Counter />
       </div>
-        
-        <div className="App" style={{ justifyContent: 'center' ,paddingLeft:'10px'}}>
-        <h1>Fun facts about React!</h1>
-        <ul>
-            <li>Was first release in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 200K stars on GitHub</li>
-            <li>Is maintained by Meta</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-        </div>
-        </div>
-      </>
-    </main>
+
+      <footer>
+        <p>©2025 Nuthan Kishore development. All rights reserved.</p>
+      </footer>
+    </div>
+    
   );
 }
 
